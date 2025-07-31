88807fb62c806e930ec4e00562eb28ba2762aad9
MrKirill <sklipiskirill@gmail.com>
2025-07-31 22:34:39 +0300

    desktop version

---
+++ server.js
@@ -0,0 +1,18 @@
+// cd "C:\Users\Користувач\Desktop\Paws-n-Play"
+// nodemon server.js
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
