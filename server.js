4ceffad2ae954c7358d74564a3a5a021df0b9394
MrKirill <sklipiskirill@gmail.com>
2025-08-01 18:05:11 +0300

    update header bg

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
